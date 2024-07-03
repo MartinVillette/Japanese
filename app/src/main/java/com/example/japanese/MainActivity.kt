722548b49d7c1181f8beb37a9ecdfe1b3624b80d
Martin Villette <martin.villette@etu.ec-lyon.fr>
2024-07-03 14:42:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/japanese/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.japanese
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    val GROQ_API_KEY = "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"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val url = "https://api.groq.com/openai/v1/chat/completions"
+        val userPrompt = "What is your favorite color ?"
+
+        val jsonBody = JSONObject()
+        jsonBody.put("model", "llama3-70b-8192")
+        val messagesArray = JSONArray()
+        val userMessage = JSONObject()
+        userMessage.put("role", "user")
+        userMessage.put("content", userPrompt)
+
+        messagesArray.put(userMessage)
+
+        jsonBody.put("messages", messagesArray)
+
+        val requestBody = jsonBody.toString().toRequestBody("application/json".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Authorization", "Bearer $GROQ_API_KEY")
+            .post(requestBody)
+            .build()
+
+        val okHttpClient = OkHttpClient()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle network errors
+                runOnUiThread {
+                    Log.e("API Error", "Error: ${e.message}")
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body
+                if (response.isSuccessful && responseBody != null){
+                    // Handle successful response
+                    val responseJson = JSONObject(responseBody.string())
+                    val message = responseJson.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content")
+                    // Handle API response and update UI
+                    runOnUiThread {
+                        Log.d("API Response", message)
+                    }
+                } else {
+                    // Handle unsuccessful response
+                    runOnUiThread {
+                        Log.e("API Error", "Error: ${response.code}")
+                    }
+                    return
+                }
+            }
+        })
+    }
+}
